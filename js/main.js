cf0a26048e2847d52077ac1b9db8086fed1b4d06
TommasoPezzola <pezzola123@gmail.com>
2023-04-14 16:00:15 +0200

    Milestone 0.9

---
+++ js/main.js
@@ -0,0 +1,43 @@
+const tariffa = 0.21;
+
+let nome = document.getElementById("nomeUtente");
+
+let distanza = document.getElementById("distanza");
+
+let eta = document.getElementById("eta");
+
+let genera = document.getElementById("genera");
+
+let passeggero = document.getElementById("passeggero");
+
+let offerta = document.getElementById("offerta");
+
+let carrozza = document.getElementById("carrozza");
+
+let codice = document.getElementById("codice");
+
+let costo = document.getElementById("costo");
+
+let prezzo = distanza.value * tariffa;
+
+genera.addEventListener("click",
+    function() {
+       passeggero.innerText = nome.value;
+
+       if (eta.value == "Minorenne") {
+        offerta.innerText = "Biglietto Scontato"
+        prezzo *= 0.8 
+       } else if (eta.value == "Over 65") {
+        offerta.innerText = "Biglietto Scontato"
+        prezzo *= 0.6
+       } else {
+        offerta.innerText = "Biglietto Standard"
+       }
+
+       carrozza.innerText = Math.ceil(Math.random() * 10);
+
+       codice.innerText = Math.ceil(Math.random() * 100000);
+
+       costo.innerText = prezzo;
+    }
+    )
